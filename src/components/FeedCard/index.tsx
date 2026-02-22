fb84df16145d34f06283e129391f42b74cd57044
haim-autonomyai <haim@autonomyai.io>
2026-02-22 23:27:11 +0200

    Remove margin from FeedCard content

--- src/components/FeedCard/index.tsx
+++ src/components/FeedCard/index.tsx
@@ -11,7 +11,7 @@ function FeedCard(props: FeedProps) {
       h-96
       ">
 
-      <div className="p-4">
+      <div className="p-4 m-0">
         <h2 className="text-lg font-medium text-gray-900">{props.title}</h2>
         <p className="mt-2 text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
